21c55f2bb19112ff7d2bfb3e18f235f950d90de5
Waleed Abouheif <wabouheif88@gmail.com>
2023-01-19 05:40:59 +0200

    Update favorite_places_nested_dict_list.py

--- favorite_places_nested_dict_list.py
+++ favorite_places_nested_dict_list.py
@@ -1,4 +1,3 @@
-#Nested List inside a nested Dictionary
 favorite_places = {
     'john' : {
         'favorite_places': ['rome', 'berlin', 'paris'],
@@ -9,15 +8,15 @@ favorite_places = {
         'wishlist': ['germany', 'italy'],
     }
 }
-#Looping Through the Dictionary Keys
-for keys in favorite_places.keys():
-    fav_places = favorite_places[keys].get('favorite_places')
-    wishlist_places = favorite_places[keys].get('wishlist')
+
+for person_name in favorite_places.keys():
+    fav_places = favorite_places[person_name]['favorite_places']
+    wishlist_places = favorite_places[person_name]['wishlist']
     
-    print(f"\n{keys.title()}'s Favorite Places Are:")
-    for fav_place in fav_places: #Looping through the list values inside nested 'favorite_places' Key
+    print(f"\n{person_name.title()}'s Favorite Places Are:")
+    for fav_place in fav_places: 
         print(f"\t\t\t{fav_place.title()}")
 
     print(f"\n    And Wishlist Places Are:")
-    for wishlist_place in wishlist_places: #Looping through the list values inside nested 'wishlist' Key
+    for wishlist_place in wishlist_places:
         print(f"\t\t\t{wishlist_place.title()}")
